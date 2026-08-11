f811e35a9d610ab3b9e7ed227066482b4aa48c83
alexlaflor <52672618+alexlaflor@users.noreply.github.com>
2026-08-11 12:18:23 -0300

    Create page.js

---
+++ app/produtos/pipe-jacking/page.js
@@ -0,0 +1,6 @@
+import ProductDetail from '../../components/ProductDetail';
+export const metadata = {
+  title: 'Pipe jacking',
+  description: 'Sistemas de pipe jacking e microtúnel para instalação de tubulações sem vala.',
+};
+export default function Page() { return <ProductDetail slug="pipe-jacking" />; }
