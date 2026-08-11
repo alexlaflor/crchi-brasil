dc301f37d01b0572c717736791ebf7df5dd14ffb
alexlaflor <52672618+alexlaflor@users.noreply.github.com>
2026-08-11 12:19:09 -0300

    Create page.js

---
+++ app/produtos/ferrovias/page.js
@@ -0,0 +1,6 @@
+import ProductDetail from '../../components/ProductDetail';
+export const metadata = {
+  title: 'Ferrovias',
+  description: 'Equipamentos para construção e manutenção ferroviária.',
+};
+export default function Page() { return <ProductDetail slug="ferrovias" />; }
